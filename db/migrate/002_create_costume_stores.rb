af0350c33d1933d370d8b7b7498af2b3705ffa2c
Katleiah Ramos <katleiahramos@gmail.com>
2018-05-14 16:10:15 +0000

    automatically backed up by learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,6 +1,12 @@
 # Create your costume_stores migration here
 
-class CostumeStore
+class CostumeStore < ActiveRecord::Migration[4.2]
+  def change
+    create_table :costume_stores
+
+
+
+  end
 
 
 end
