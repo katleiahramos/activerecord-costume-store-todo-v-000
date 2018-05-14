89d78077bd5176ddf194508328437a6594b0c1fe
Katleiah Ramos <katleiahramos@gmail.com>
2018-05-14 16:23:12 +0000

    automatically backed up by learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -7,7 +7,7 @@ class CreateHauntedHouses < ActiveRecord::Migration[4.2]
       t.string :location
       t.string :theme
       t.integer :price
-      t.boolean :is_family_friendly
+      t.boolean :family_friendly
       t.timestamp
       t.string :description 
 
