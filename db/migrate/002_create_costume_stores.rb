f8d8c8de2bb57e1e9c4b841b8db4ebfd9bdbe733
Katleiah Ramos <katleiahramos@gmail.com>
2018-05-14 16:12:35 +0000

    automatically backed up by learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -2,7 +2,14 @@
 
 class CostumeStore < ActiveRecord::Migration[4.2]
   def change
-    create_table :costume_stores
+    create_table :costume_stores do |t|
+      t.string :name
+      t.string :location
+      t.integer :costume_inventory
+      t.integer :num_of_employees
+      t.boolean :still_in_business
+      t.integer :start_time
+      t.integer :end_time
 
 
 
